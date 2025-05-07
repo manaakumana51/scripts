cc043ffd8876951aa4cde8ebca4cf653e6ef70cc
manaakumana51 <manaakumana51@gmail.com>
2025-05-07 17:08:54 +0700

    Create spaceman.js

---
+++ spaceman.js
@@ -0,0 +1,45 @@
+function _0x2c17(_0x4acae6, _0x5c8a6d) {
+    const _0x4ff473 = _0x4ff4();
+    return _0x2c17 = function (_0x2c1786, _0x500a5d) {
+        _0x2c1786 = _0x2c1786 - 0xcf;
+        let _0x5ac5a7 = _0x4ff473[_0x2c1786];
+        return _0x5ac5a7;
+    }, _0x2c17(_0x4acae6, _0x5c8a6d);
+}
+
+function _0x4ff4() {
+    const _0x2a544b = ['202242zZHJoo', '5DiWlsM', 'trim', '377AvfBXe', '330QTPIeH', 'insertAdjacentElement', 'href', '1131627YqEaZS', '1006390ycVYfO', '9WwdFzN', 'find', '\x0a            <img alt=\"Spaceman\" height=\"25\" loading=\"lazy\"\x0a                src=\"https://clickdrive.tech/storage/cgg/PlanetBola88/PLE-SPACEMAN-300.webp\"\x0a                style=\"--image-src:url(//clickdrive.tech/storage/cgg/PlanetBola88/PLE-SPACEMAN-300.webp)\"\x0a                width=\"25\"> Spaceman\x0a        ', 'addEventListener', 'textContent', 'main a[data-game-category=\"Unknown\"]', 'querySelectorAll', '447344WtBoEj', 'from', '1494152mDbhws', 'DOMContentLoaded', 'innerHTML', 'Hot Games', '3092502SsSspf', 'createElement', 'querySelector'];
+    _0x4ff4 = function () {
+        return _0x2a544b;
+    };
+    return _0x4ff4();
+}
+const _0x523f22 = _0x2c17;
+(function (_0x272c0f, _0x607ffd) {
+    const _0x1209de = _0x2c17,
+        _0x1ad156 = _0x272c0f();
+    while (!![]) {
+        try {
+            const _0xfed3d8 = parseInt(_0x1209de(0xd9)) / 0x1 * (parseInt(_0x1209de(0xda)) / 0x2) + parseInt(_0x1209de(0xd6)) / 0x3 + parseInt(_0x1209de(0xcf)) / 0x4 + parseInt(_0x1209de(0xd7)) / 0x5 * (-parseInt(_0x1209de(0xd3)) / 0x6) + parseInt(_0x1209de(0xdd)) / 0x7 + parseInt(_0x1209de(0xe6)) / 0x8 + parseInt(_0x1209de(0xdf)) / 0x9 * (parseInt(_0x1209de(0xde)) / 0xa);
+            if (_0xfed3d8 === _0x607ffd) break;
+            else _0x1ad156['push'](_0x1ad156['shift']());
+        } catch (_0x1cac3c) {
+            _0x1ad156['push'](_0x1ad156['shift']());
+        }
+    }
+}(_0x4ff4, 0x4ab26), document[_0x523f22(0xe2)](_0x523f22(0xd0), function () {
+    const _0x4fc1b2 = _0x523f22,
+        _0x270f9f = document[_0x4fc1b2(0xd5)]('.top-menu'),
+        _0x37b8c2 = Array[_0x4fc1b2(0xe7)](_0x270f9f[_0x4fc1b2(0xe5)]('li'))[_0x4fc1b2(0xe0)](_0x17e690 => _0x17e690['querySelector']('a') && _0x17e690[_0x4fc1b2(0xd5)]('a')[_0x4fc1b2(0xe3)][_0x4fc1b2(0xd8)]() === _0x4fc1b2(0xd2));
+    if (_0x37b8c2) {
+        const _0x8fa510 = document[_0x4fc1b2(0xd4)]('li');
+        _0x8fa510[_0x4fc1b2(0xd1)] = '\x0a                <a href=\"javascript:openNewTab(\'/dispatch/game/PPLIVECASINO/Mobile/PPLiveCasino_1301\', \'CrashGame\')\">\x0a                    <img alt=\"Spaceman\" height=\"30\" loading=\"lazy\"\x0a                        src=\"https://clickdrive.tech/storage/cgg/PlanetBola88/PLE-SPACEMAN-300.webp\"\x0a                        style=\"--image-src:url(//clickdrive.tech/storage/cgg/PlanetBola88/PLE-SPACEMAN-300.webp)\"\x0a                        width=\"30\"> Spaceman </a>\x0a            ', _0x37b8c2[_0x4fc1b2(0xdb)]('afterend', _0x8fa510);
+    }
+}), document[_0x523f22(0xe2)]('DOMContentLoaded', function () {
+    const _0x3c8a74 = _0x523f22,
+        _0x11c742 = document[_0x3c8a74(0xd5)](_0x3c8a74(0xe4));
+    if (_0x11c742) {
+        const _0x29a7af = document[_0x3c8a74(0xd4)]('a');
+        _0x29a7af[_0x3c8a74(0xdc)] = 'javascript:openNewTab(\'/dispatch/game/PPLIVECASINO/Mobile/PPLiveCasino_1301\', \'CrashGame\')', _0x29a7af[_0x3c8a74(0xd1)] = _0x3c8a74(0xe1), _0x11c742[_0x3c8a74(0xdb)]('afterend', _0x29a7af);
+    }
+}));
